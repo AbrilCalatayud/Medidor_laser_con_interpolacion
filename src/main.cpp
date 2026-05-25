1b8fab851122715d6bc1c7882158fed157dc305f
AbrilCalatayud <abril.calatayud.contacto@gmail.com>
2026-05-24 23:14:03 -0300

    Calculo de la contribución del ambiente y setup de los pines

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,21 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+//cambiar cuando sepa a qué pines los voy a conectar
+const int pinTransistor = A0;
+const int pinLaser = A1;
+
+int contribucionAmbiente = 0;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  int tensionSinLaser = analogRead(pinTransistor);
+
+  pinMode(pinLaser, OUTPUT);
+  digitalWrite(pinLaser, HIGH);
+
+  int tensionConLaser = analogRead(pinTransistor);
+
+  contribucionAmbiente = tensionConLaser - tensionSinLaser;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
